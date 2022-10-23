7de2e1d68ae7642a63c84efb151609f0b019a99c
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-10-23 21:10:19 +0900

    20221023 Add Route

--- src/main/kotlin/net/konohana/sakuya/maihime/crud/sample/router/MaihimeSampleRouter.kt
+++ src/main/kotlin/net/konohana/sakuya/maihime/crud/sample/router/MaihimeSampleRouter.kt
@@ -2,7 +2,9 @@ package net.konohana.sakuya.maihime.crud.sample.router
 
 import io.ktor.server.routing.Route
 import net.konohana.sakuya.maihime.crud.sample.controller.maihimeSampleController
+import net.konohana.sakuya.maihime.crud.sample.controller.oneway.onewayTicketAdultRequestController
 
 fun Route.maihimeSampleRouter() {
     maihimeSampleController()
+    onewayTicketAdultRequestController()
 }
