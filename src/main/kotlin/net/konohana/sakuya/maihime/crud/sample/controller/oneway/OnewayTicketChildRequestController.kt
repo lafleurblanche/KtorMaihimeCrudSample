5258fc91c1cb1630a1376ce92fb61bc1f522b5c1
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-10-23 21:56:31 +0900

    20221023 NEW

---
+++ src/main/kotlin/net/konohana/sakuya/maihime/crud/sample/controller/oneway/OnewayTicketChildRequestController.kt
@@ -0,0 +1,50 @@
+package net.konohana.sakuya.maihime.crud.sample.controller.oneway
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.call
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.route
+import net.konohana.sakuya.maihime.crud.sample.domain.service.OnewayTicketChildRequestService
+
+fun Route.onewayTicketChildRequestController() {
+    route("oneway") {
+        route("child") {
+            post {
+                val request = call.receive<OnewayTicketChildRequestDataFromAPI>()
+                val id = OnewayTicketChildRequestService.create(
+                    request.requestNo,
+                    request.ticketNo,
+                    request.fromSta,
+                    request.toSta,
+                    request.via,
+                    request.fare,
+                    request.dayOfUse
+                )
+                call.respond(mapOf("id" to id))
+            }
+            get("/{id}") {
+                val id = call.parameters["id"]?.let { it.toInt() } ?: run {
+                    return@get call.respond(HttpStatusCode.BadRequest, "IDが指定されていません")
+                }
+                val onewayTicketChildReq = OnewayTicketChildRequestService.find(id) ?: run {
+                    return@get call.respond(HttpStatusCode.NotFound, "データが存在しません id: $id")
+                }
+                call.respond(onewayTicketChildReq)
+            }
+        }
+    }
+}
+
+data class OnewayTicketChildRequestDataFromAPI(
+    val requestNo: String,
+    val ticketNo: String,
+    val fromSta: String,
+    val toSta: String,
+    val via: String,
+    val fare: String,
+    val dayOfUse: String,
+)
