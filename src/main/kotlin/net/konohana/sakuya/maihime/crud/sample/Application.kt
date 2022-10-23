5e922a9512ac18f115b8a5583a1222f29e963820
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-10-23 21:10:05 +0900

    20221023 DB接続処理追加

--- src/main/kotlin/net/konohana/sakuya/maihime/crud/sample/Application.kt
+++ src/main/kotlin/net/konohana/sakuya/maihime/crud/sample/Application.kt
@@ -1,6 +1,7 @@
 package net.konohana.sakuya.maihime.crud.sample
 
 import io.ktor.server.application.Application
+import net.konohana.sakuya.maihime.crud.sample.db.initArisuDB
 import net.konohana.sakuya.maihime.crud.sample.plugins.configureHTTP
 import net.konohana.sakuya.maihime.crud.sample.plugins.configureRouting
 import net.konohana.sakuya.maihime.crud.sample.plugins.configureSerialization
@@ -11,6 +12,10 @@ fun main(args: Array<String>): Unit =
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    val url = environment.config.property("ktor.arisudb.uri").getString()
+    val username = environment.config.property("ktor.arisudb.username").getString()
+    val password = environment.config.property("ktor.arisudb.password").getString()
+    initArisuDB(url, username, password)
     configureSerialization()
     configureHTTP()
     configureRouting()
