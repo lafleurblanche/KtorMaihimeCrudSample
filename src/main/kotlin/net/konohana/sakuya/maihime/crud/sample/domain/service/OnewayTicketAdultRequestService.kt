7ce14bb7abbac341490ba404fc756ca7145e6871
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-10-23 21:10:52 +0900

    20221023 NEW

---
+++ src/main/kotlin/net/konohana/sakuya/maihime/crud/sample/domain/service/OnewayTicketAdultRequestService.kt
@@ -0,0 +1,24 @@
+package net.konohana.sakuya.maihime.crud.sample.domain.service
+
+import net.konohana.sakuya.maihime.crud.sample.data.oneway.OnewayTicketAdultRequestRepository
+
+object OnewayTicketAdultRequestService {
+    fun create(
+        requestNo: String,
+        ticketNo: String,
+        fromSta: String,
+        toSta: String,
+        via: String,
+        fare: String,
+        dayOfUse: String,
+    ) = OnewayTicketAdultRequestRepository.create(
+        requestNo,
+        ticketNo,
+        fromSta,
+        toSta,
+        via,
+        fare,
+        dayOfUse
+    )
+    fun find(id: Int) = OnewayTicketAdultRequestRepository.find(id)
+}
