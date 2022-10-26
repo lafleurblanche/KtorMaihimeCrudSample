608fa15d81bd1675ab6f8d046bf26f0fe7d1b60c
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-10-26 22:09:40 +0900

    20221026 Add Route

--- src/main/kotlin/net/konohana/sakuya/maihime/crud/sample/controller/MaihimeSampleController.kt
+++ src/main/kotlin/net/konohana/sakuya/maihime/crud/sample/controller/MaihimeSampleController.kt
@@ -6,6 +6,7 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import net.konohana.sakuya.maihime.crud.sample.controller.oneway.OnewayTicketAdultRequestDataFromAPI
 import net.konohana.sakuya.maihime.crud.sample.dao.request.MaihimeSampleRequest
 import net.konohana.sakuya.maihime.crud.sample.dao.response.MaihimeSampleResponse
 
@@ -23,6 +24,22 @@ fun Route.maihimeSampleController() {
                 val param = call.receive<MaihimeSampleRequest>()
                 println(param.sampleValue)
                 call.respond(MaihimeSampleResponse("BadRequest", "リクエスト不正"))
+            }
+        }
+        route("oneway") {
+            route("adult") {
+                route("sample") {
+                    post {
+                        val req = call.receive<OnewayTicketAdultRequestDataFromAPI>()
+                        println(req.requestNo)
+                        println(req.ticketNo)
+                        println(req.fromSta)
+                        println(req.toSta)
+                        println(req.via)
+                        println(req.fare)
+                        println(req.dayOfUse)
+                    }
+                }
             }
         }
     }
